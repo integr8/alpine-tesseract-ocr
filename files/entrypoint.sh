59c3c513e93c6501ca62ba80ce14bc83ecb0a537
Fábio Luciano <fabio@naoimporta.com>
2018-12-30 17:42:21 -0200

    Optimizing image for ocr

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-convert $1 -colorspace Gray -density 300 -depth 8 -background white -alpha Off /tmp/ocr.tif
+convert $1 -colorspace Gray -resize 400% -density 300 -depth 8 -background white -alpha Off /tmp/ocr.tif
 tesseract /tmp/ocr.tif -l por stdout quiet | sed '/^[[:space:]]*$/d'
